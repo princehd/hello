5e8600dea98616357b716b8b0703ab21677a8d97
goehehgo <goehehgo@naver.com>
2017-10-18 07:21:03 +0900

    second

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -3,7 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello");
+		int a = 3;
+		System.out.println(a);
 	}
 
 }
