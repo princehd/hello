e408afd732dc1c26bedb33d74ebde5e7cd845b5c
goehehgo <goehehgo@naver.com>
2017-10-18 07:22:28 +0900

    third

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
-		int a = 3;
-		System.out.println(a);
+		System.out.println("ccc");
+		float b = 3.14f;
+		
 	}
 
 }
